5a17edf773bc4d43d84751c92e38f8b8fb15e95e
Ulrich Wohlfeil <ulrich.wohlfeil@ecmind.ch>
2025-05-06 08:51:33 +0200

    https://community.apryse.com/t/accessiblereadingordermanager-event-when-preprocessing-is-finished/11484

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import WebViewer, { Core } from '@pdftron/webviewer'
+import "./style.css"
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div id="viewer"></div>
+`
+const element = document.getElementById('viewer');
+
+WebViewer({
+  path: '/webviewer', 
+  initialDoc: 'Well-Tagged-PDF-WTPDF-1.0.pdf',
+  accessibleMode: true
+}, element!).then((instance) => {
+  const {documentViewer} = instance.Core;
+  const accessibleReadingOrderManager = (documentViewer as any).getAccessibleReadingOrderManager() as Core.AccessibleReadingOrderManager; // eslint-disable-line  - Bug in TS Library
+  
+  accessibleReadingOrderManager.addEventListener("accessibleReadingOrderModeReady", () => {
+    console.log("accessibleReadingOrderModeReady called");
+  });
+})
